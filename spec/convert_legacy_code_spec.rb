88dccf2d42050dbe5d522b55a62244900ed2ef2e
Willem Larsen <willemlarsen@gmail.com>
2014-07-09 15:54:25 -0700

    switch multiple keys for their values

--- spec/convert_legacy_code_spec.rb
+++ spec/convert_legacy_code_spec.rb
@@ -5,4 +5,9 @@ describe('switcharoo') do
   it('reverses the key and the value in an object with one key') do
     expect(switcharoo({ 1 => "A"})).to eq({ "A" => 1})
   end
+  it('reverses the key and the value in an object with multiple keys') do
+    expect(switcharoo({1 => "A", 2 => "B"})). to eq({"A" => 1, "B" => 2})
+  end
 end
+
+
